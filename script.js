b60ea4cf2b2409d912e2d3d5137e07bc19f4fe2a
Tech and Tonic <aaroncrume@gmail.com>
2026-01-12 19:52:47 -0800

    Implement click event logging for sectors
    Added click event logging for sector navigation.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+   <script>
+        // Optional: Add simple analytics or interaction logging here
+        document.querySelectorAll('.sector').forEach(item => {
+            item.addEventListener('click', event => {
+                console.log('Navigating to sector: ' + event.target.parentNode.getAttribute('href'));
+            })
+        });
+    </script>
